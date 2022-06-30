e7fb417b159baf1a419b58f9af9af7d7320c2448
oleganik <108150620+oleganik@users.noreply.github.com>
2022-06-30 22:09:29 +0300

    Update segment.py

--- segment.py
+++ segment.py
@@ -12,7 +12,7 @@ def process(image_file):
     image = Image.open(image_file).convert('L') # Открываем обрабатываемый файл
     resized_image = image.resize((INPUT_SHAPE[1], INPUT_SHAPE[0])) # Изменяем размер изображения в соответствии со входом сети
     array = np.array(resized_image, dtype='float64') # Меняем размерность тензора для подачи в сеть
-    array = np.expand_dims(array, axis=0)
+    array = array.reshape(-1, 28, 28, 1)
     cls_image = np.argmax(model.predict(array))
 
     return cls_image
